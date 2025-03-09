f46257c7b76e8d2cb9c125376c8f500cfff515e5
RiinSeresuto <renebaribal@gmail.com>
2025-03-09 18:35:12 +0800

    configured pages

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Welcom to nuxt</h1>
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    useSeoMeta({
+        title: "Maku",
+        ogTitle: "Maku",
+        description: "Simple photobooth application for the web.",
+        ogDescription: "Simple photobooth application for the web.",
+        //ogImage: "https://example.com/image.png",
+    });
+</script>
+
+<template>
+    <h1 class="header--title-default">I am a moment, frozen in time.</h1>
+    <p>Create photostrips on a whim to carry memories on the wind.</p>
+</template>
+
+<style>
+    .header--title-default {
+        font-family: "Josefin Slab", serif;
+        font-optical-sizing: auto;
+        font-weight: 700;
+        font-style: normal;
+        font-size: var(--step-5);
+
+        color: var(--color-dark);
+    }
+</style>
