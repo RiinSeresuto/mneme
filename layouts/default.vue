5ab48f9cd093962f5ef9d355ce97e0584882b0c8
RiinSeresuto <renebaribal@gmail.com>
2025-03-09 18:29:09 +0800

    added layout template

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main-layout">
+        <Header />
+        <main class="container">
+            <slot />
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<style scoped>
+    .main-layout {
+        min-height: 100vh;
+        min-height: 100dvh;
+        display: grid;
+        grid-template-rows:
+            auto
+            1fr
+            auto;
+    }
+</style>
