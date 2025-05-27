5ec61634d829a3b987ea5cb3d2c0e753682c629a
RiinSeresuto <renebaribal@gmail.com>
2025-05-27 13:41:24 +0800

    added SEO

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -18,6 +18,14 @@
     onMounted(() => {
         fetchImages();
     });
+
+    useSeoMeta({
+        title: "Mneme | Gallery",
+        ogTitle: "Mneme | Gallery",
+        description: "Simple photobooth application for the web.",
+        ogDescription: "Simple photobooth application for the web.",
+        //ogImage: "https://example.com/image.png",
+    });
 </script>
 
 <template>
