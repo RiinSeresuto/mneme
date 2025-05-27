393f44acc3b7356c7349c1fc9a1fe7133f6bc682
RiinSeresuto <renebaribal@gmail.com>
2025-05-27 12:59:31 +0800

    added data save and fetch

--- components/Camera.vue
+++ components/Camera.vue
@@ -1,10 +1,13 @@
 <script setup>
-    import { db } from "../db";
+    import { nanoid } from "nanoid";
+    import { db } from "../lib/db";
 
     const video = ref(null);
     const snap = ref(null);
     const canvas = ref(null);
-    let context = null;
+
+    var thumbnailsrc = ref([]);
+    var context = null;
 
     const constraints = {
         audio: false,
@@ -30,13 +33,40 @@
         }
     };
 
+    const generateFilename = () => {
+        const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+
+        return `image_${timestamp}`;
+    };
+
+    const fetchImages = async () => {
+        thumbnailsrc.value = await db.images
+            .orderBy("date_created")
+            .reverse()
+            .limit(5)
+            .toArray();
+    };
+
     const clicked = () => {
         if (context && video.value) {
             context.drawImage(video.value, 0, 0, 600, 400);
         }
 
-        // uuid
-        blob = canvas.value.toDataURL("image/png");
+        if (canvas.value.toDataURL("image/png")) {
+            db.images
+                .add({
+                    id: nanoid(),
+                    filename: generateFilename(),
+                    date_created: new Date()
+                        .toISOString()
+                        .slice(0, 19)
+                        .replace("T", " "),
+                    blob: canvas.value.toDataURL("image/png"),
+                })
+                .then(() => {
+                    fetchImages();
+                });
+        }
     };
 
     onMounted(() => {
@@ -46,22 +76,39 @@
             context = canvas.value.getContext("2d");
         }
 
+        fetchImages();
         webcam();
     });
 </script>
 
 <template>
-    <video
-        ref="video"
-        id="video"
-        style="width: 600px; height: 400px"
-        autoplay
-    ></video>
+    <div id="camera-view">
+        <video
+            ref="video"
+            id="video"
+            style="width: 600px; height: 400px"
+            autoplay
+        ></video>
+    </div>
+
     <div ref="snap" class="snap" @click="clicked">
         <i class="bx bx-camera"></i> Snap
     </div>
 
-    <canvas ref="canvas" width="600px" height="400px"></canvas>
+    <div class="thumbnail-view">
+        <div v-for="img in thumbnailsrc" :key="img.id" class="thumbnail">
+            <img :src="img.blob" height="100%" width="100%" />
+        </div>
+
+        <div class="thumbnail">Gallery</div>
+    </div>
+
+    <canvas
+        ref="canvas"
+        width="600px"
+        height="400px"
+        style="display: none"
+    ></canvas>
 </template>
 
 <style scoped>
@@ -74,5 +121,30 @@
         justify-content: center;
         align-items: center;
         cursor: pointer;
+        margin-top: 16px;
+        margin-inline: auto;
+    }
+
+    .thumbnail-view {
+        margin-top: 16px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        gap: 8px;
+    }
+
+    .thumbnail {
+        background-color: gray;
+        height: 75px;
+        width: 100px;
+        border-radius: 8px;
+        overflow: hidden;
+    }
+
+    #camera-view {
+        width: 100%;
+
+        display: flex;
+        justify-content: center;
     }
 </style>
